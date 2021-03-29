08fe69f2cfdeb3418476b09dbc2232a5085ca315
skysamer <skyslayer123@naver.com>
2021-03-29 11:22:23 +0900

    twoSum

---
+++ src/ch7/TwoSum.java
@@ -0,0 +1,31 @@
+package ch7;
+
+import java.util.Arrays;
+
+public class TwoSum {
+	
+	 public static int[] twoSum(int[] nums, int target) {
+		 int[] arr=new int[2];
+		 for(int i=0; i<nums.length-1; i++) {
+			 for(int j=i+1; j<nums.length; j++) {
+				 if(nums[i]+nums[j]==target) {
+					 arr[0]=i;
+					 arr[1]=j;
+					 break;
+				 }
+			 }
+		 }
+		 
+	     return arr;   
+	 }
+
+	public static void main(String[] args) {
+		int[] nums= {2, 7, 11, 15};
+		int target=9;
+		
+		int[] arr=twoSum(nums, target);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
